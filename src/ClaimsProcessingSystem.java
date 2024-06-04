41aebecaf654ee46ba8efa470f5276ab78ac1fa8
SamuelCallWind <samcallwind@gmail.com>
2024-06-04 21:31:51 +0200

    buttons & input fields added

--- src/ClaimsProcessingSystem.java
+++
@@ -1,6 +0,0 @@
-public class ClaimsProcessingSystem {
-
-    public static void main(String args[]) {
-        System.out.println("Hello world!");
-    }
-}
--- src/LoginSystem.java
+++ src/LoginSystem.java
@@ -1,10 +1,43 @@
 import java.awt.*;
-import javax.swing.*;
 
-public class LoginSystem {
+class LoginSystem {
 
     int height = 600;
     int width = 500;
 
+    LoginSystem() {
 
+        Frame frame = new Frame();
+
+        Label l = new Label("employee id:");
+
+        Button b = new Button("Submit");
+
+        TextField t = new TextField();
+
+        l.setBounds(50, 50, 100, 30);
+        b.setBounds(50, 150, 100, 30);
+        t.setBounds(50, 100, 200, 30);
+
+        frame.add(l);
+        frame.add(t);
+        frame.add(b);
+
+        frame.setTitle("Login");
+
+        frame.setSize(width, height);
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+    }
+
+    public static void main(String args[]) {
+
+        LoginSystem newLogin = new LoginSystem();
+
+
+    }
 }
+
